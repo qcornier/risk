7c5b3c9639537a41ccbe9c47cf3e2b2900fca35b
parade14 <ppaul.bellamy@gmail.com>
2018-06-02 19:45:43 +0200

    add marker method updated

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,53 +1,122 @@
 package map;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.geo.Location;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.utils.GeoUtils;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import model.Territoire;
+import model.unite.Canon;
+import model.unite.Cavalier;
+import model.unite.Unite;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
 import processing.event.MouseEvent;
-import de.fhpotsdam.unfolding.UnfoldingMap;
-import de.fhpotsdam.unfolding.data.Feature;
-import de.fhpotsdam.unfolding.data.GeoJSONReader;
-import de.fhpotsdam.unfolding.marker.Marker;
-import de.fhpotsdam.unfolding.marker.MarkerManager;
-import de.fhpotsdam.unfolding.providers.MBTilesMapProvider;
-import de.fhpotsdam.unfolding.utils.MapUtils;
-import de.fhpotsdam.unfolding.geo.Location;
-import de.fhpotsdam.unfolding.utils.GeoUtils;
+import util.JeuUtil;
 
-public class Map extends PApplet {
+public class InterfaceGraphiqueUtil extends PApplet {
+	
+	private static final Color[] tabCouleur = {Color.BLUE, Color.RED, Color.GREEN, 
+											   Color.CYAN, Color.ORANGE, Color.PINK};
 	private static final long serialVersionUID = 1L;
 	UnfoldingMap map;
 	List<Marker> countryMarkers;
 	Location lisbonLocation = new Location(38.71f, -9.14f);
 	
-	 
-    public void setup() {
-        size(800, 600);
-        HashMap locations = new HashMap(42);
-        String tilesStr = sketchPath("../risk-develop/resources/background.mbtiles");
+	
+	public void setup() {
+		size(800, 600);
         map = new UnfoldingMap(this);
         List<Feature> countries = GeoJSONReader.loadData(this, "../risk-develop/resources/map.geojson");
         countryMarkers = MapUtils.createSimpleMarkers(countries);
         map.addMarkers(countryMarkers);
         MapUtils.createDefaultEventDispatcher(this, map);
         map.setBackgroundColor(6002415);
-        
+	}
+	
+	
+	public void placerUnite(Territoire territoire, Unite unite) {
+		
+		
+		String nomTerritoire = territoire.getNom();
+		
+		// tu recuperes l'id du joueur
+		int idJoueur = territoire.getOccupant().getId();
+		// tu chopes la couleur du pion en faisant l'association avec la position dans le tableau de couleur
+		Color couleurPion = tabCouleur[idJoueur];
+		
+		for (Marker marker : countryMarkers) {
+			if (marker.getProperty("name") == nomTerritoire) {
+				Location markerCentre = GeoUtils.getCentroid(marker);
+				break;
+			}
+		}
+		
+		
+		if(unite.getClass().equals(Canon.class)) {
+			PImage imageUnite = this.loadImage("../risk-develop/resources/canon.png");
+		} else if(unite.getClass().equals(Cavalier.class)) {
+			PImage imageUnite = this.loadImage("../risk-develop/resources/chevalier.png");
+		} else {
+			PImage imageUnite = this.loadImage("../risk-develop/resources/soldat.png");
+		}
+		
+		imageUnite.resize(30, 30);
+        ImageMarker markerUnite = new ImageMarker(markerCentre, imageUnite);
+        map.addMarker(markerUnite);
+	}
+	
+	public static void supprimerUnite(String nomTerritoire, Unite unite) {
+		if(unite.getClass().equals(Canon.class)) {
+			// supprime un pion canon
+			
+		} else if(unite.getClass().equals(Cavalier.class)) {
+			// supprime un pion cavalier
+		} else {
+			// supprime un pion soldat
+		}
+	}
+	
+	// rÃ©cupÃ¨re le nom de l'etat clique
+	public static String recupererNomTerritoireClique() {
+		return "";
+	}
+	
+	// retourner pays adjacents
+	public static List<String> getPaysAdjacent(String nomPays) {
+		
+		List<String> listeAdjacents = new ArrayList<>();
+		listeAdjacents.add("");
+		
+		
+		return listeAdjacents;
+	}
+	
+	// rÃ©cupÃ¨re le nb de joueurs sÃ©lectionnÃ© en debut de partie
+	public static int getNbJoueurs() {
+		return 0;
+	}
+	
+	public void mouseReleased(MouseEvent event) {
+    	int mouseButton = event.getButton();
+    	if (mouseButton == PConstants.LEFT) {
+    		System.out.println("Bouton gauche cliqué");
+    	}
     }
- 
-    public void draw() {
-        map.draw();
-        Marker hitMarker = map.getFirstHitMarker(mouseX, mouseY);
-        PImage chevalier = loadImage("../risk-develop/resources/knight.png");
-        chevalier.resize(30, 30);
-        ImageMarker imgMarker1 = new ImageMarker(lisbonLocation, chevalier);
-        map.addMarker(imgMarker1);
-        MarkerManager<Marker> last = new MarkerManager();
-        MarkerManager test = map.getLastMarkerManager();
-        //map.removeMarkerManager(test);
-        int index = countryMarkers.indexOf(hitMarker);
+	
+	public static Location getCentreTerritoire(Territoire territoire) {
+		
+	}
+
+}
         //Location markerCentroid = GeoUtils.getCentroid(countryMarkers.get(0));
         //System.out.println(markerCentroid);
 
