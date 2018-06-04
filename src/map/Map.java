752342a81796cc1311ac4630f47d750ac50561d2
tiboxman <tibogrouet@gmail.com>
2018-06-04 20:41:41 +0200

    fix map interraction

--- src/map/Map.java
+++ src/map/Map.java
@@ -10,6 +10,8 @@ import de.fhpotsdam.unfolding.data.Feature;
 import de.fhpotsdam.unfolding.data.GeoJSONReader;
 import de.fhpotsdam.unfolding.geo.Location;
 import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.marker.MarkerManager;
+import de.fhpotsdam.unfolding.utils.DebugDisplay;
 import de.fhpotsdam.unfolding.utils.GeoUtils;
 import de.fhpotsdam.unfolding.utils.MapUtils;
 import model.Territoire;
@@ -28,9 +30,9 @@ public class Map extends PApplet {
 											   Color.CYAN, Color.ORANGE, Color.PINK};
 	private static final long serialVersionUID = 1L;
 	private static UnfoldingMap map;
-	private List<Marker> countryMarkers;
+	private static List<Marker> countryMarkers;
+	// TODO supprimer ce jeu d'essai
 	private Location lisbonLocation = new Location(38.71f, -9.14f);
-	
 	
 	
 	public static UnfoldingMap getMap() {
@@ -42,17 +44,25 @@ public class Map extends PApplet {
 	}
 	
 	public void setup() {
-		size(1080, 700);
-        map = new UnfoldingMap(this);
+		size(1350, 680);
+        map = new UnfoldingMap(this, 0, 0, 1000, 980);
         List<Feature> countries = GeoJSONReader.loadData(this, "./resources/map.geojson");
         countryMarkers = MapUtils.createSimpleMarkers(countries);
         map.addMarkers(countryMarkers);
         MapUtils.createDefaultEventDispatcher(this, map);
         map.setBackgroundColor(6002415);
+        map.zoomToLevel(2);
+        map.setZoomRange(2, 3);
+        map.setPanningRestriction(new Location(0, 0) , 8000);
+       
+        
+        
+
+    
 	}
 	
 	public void draw(){
-		map.draw();
+		 map.draw();
 	}
 	public void placerUnite(Territoire territoire, Unite unite) {
 		String nomTerritoire = territoire.getNom();
@@ -81,15 +91,32 @@ public class Map extends PApplet {
         map.addMarker(markerUnite);
 	}
 	
-	public static void supprimerUnite(String nomTerritoire, Unite unite) {
-		if(unite.getClass().equals(Canon.class)) {
-			// supprime un pion canon
-			
-		} else if(unite.getClass().equals(Cavalier.class)) {
-			// supprime un pion cavalier
-		} else {
-			// supprime un pion soldat
+	public static void supprimerUnite(Territoire territoire, Unite unite) {
+		String nomTerritoire = territoire.getNom();
+		// tu recuperes l'id du joueur
+		int idJoueur = territoire.getOccupant().getId();
+		// tu chopes la couleur du pion en faisant l'association avec la position dans le tableau de couleur
+		Color couleurPion = tabCouleur[idJoueur];
+		Location markerCentre = null;
+		for (Marker marker : countryMarkers) {
+			if (marker.getProperty("name").equals(nomTerritoire)) {
+				markerCentre = GeoUtils.getCentroid(marker);
+				break;
+			}
 		}
+		PImage imageUnite = null;
+		if(unite.getClass().equals(Canon.class)) {
+			//imageUnite = this.loadImage("./resources/canon.png");
+		} else if(unite.getClass().equals(Cavalier.class)) {
+			//PImage imageUnite = this.loadImage("../risk-develop/resources/chevalier.png");
+		} else {
+			//PImage imageUnite = this.loadImage("../risk-develop/resources/soldat.png");
+		}
+		
+		imageUnite.resize(30, 30);
+        ImageMarker markerUnite = new ImageMarker(markerCentre, imageUnite);
+        MarkerManager makerManager = new MarkerManager(countryMarkers);
+        makerManager.removeMarker(markerUnite);
 	}
 	
 	// rÃ©cupÃ¨re le nom de l'etat clique
@@ -115,7 +142,11 @@ public class Map extends PApplet {
 	public void mouseReleased(MouseEvent event) {
     	int mouseButton = event.getButton();
     	if (mouseButton == PConstants.LEFT) {
-    		System.out.println("Bouton gauche cliqué");
+    		Marker hitMarker = map.getFirstHitMarker(event.getX(), event.getY());
+    		if (hitMarker != null ){
+        		System.out.println(hitMarker.getProperty("name"));
+        		System.out.println(event.getX() + " " + event.getY());
+    		}
     	}
     }
 	
